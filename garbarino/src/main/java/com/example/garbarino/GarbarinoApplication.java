68dc3417ceae3fa90d6158a2ac046b67d1c5435b
ariel <rivelinogol@gmail.com>
2019-10-31 10:57:10 -0300

    first setup.

---
+++ garbarino/src/main/java/com/example/garbarino/GarbarinoApplication.java
@@ -0,0 +1,13 @@
+package com.example.garbarino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbarinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarbarinoApplication.class, args);
+	}
+
+}
---
+++ garbarino/src/test/java/com/example/garbarino/GarbarinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.garbarino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbarinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
